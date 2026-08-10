6cae742903aea2e6a42e543cf23941f7d067e3ff
webbrain-one <295484252+webbrain-one@users.noreply.github.com>
2026-08-10 06:00:48 +0300

    Add Lzheandreyka shop
    Add the following items:
    - Baby fox candies
    - Korzhiki
    - Lit energy (requires passport)

---
+++ shops/lzheandreyka_shop.gd
@@ -0,0 +1,9 @@
+class_name LzheandreykaShop
+extends Resource
+
+@export var shop_name: String = "Lzheandreyka"
+@export var items: Array[Dictionary] = [
+	{"name": "Baby fox candies"},
+	{"name": "Lit energy", "requires_passport": true},
+	{"name": "Korzhiki"}
+]
